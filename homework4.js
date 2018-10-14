21828e99361e2559311d711b33ff97623f9d38a9
ArmanGy <43111149+ArmanGy@users.noreply.github.com>
2018-10-14 16:13:49 +0400

    Add files via upload

---
+++ homework4.js
@@ -0,0 +1,86 @@
+//ex 1
+
+const star =function(x) {
+  while(x>0){
+    return star(x-1)+"*";
+  }
+  x = x-1;
+ 	 return " ";
+};
+const space = function(y){
+	if(y===0){
+		return " ";
+	}
+	return space(y-1)+" ";
+};
+
+const triangleStars = function(a) {
+	const loop =function(b){
+	  while (b>0){
+	    console.log(space(a-b)+star(2*b-1));
+		loop(b-1);
+        b=b-a;
+      }
+      return ;
+};
+  return loop(a);
+};
+const k = triangleStars(4);
+
+//ex 2
+
+const a = function ([[a,c,d],[b,e,f]]){
+  return [[a,c,d]+[,b,e,f]];
+}; 
+const b = a([[1,2,3],[4,5,6]]);
+console.log(b)
+
+//ex3
+
+  const findMinMax = function (a,b){
+    if (b === true){
+      return [a[2]];
+    } else {
+    return [a[0]];
+    }
+  }; 
+  const a = findMinMax ([1,2,3],false);
+  console.log(a);
+
+//ex4
+
+const forEach = function (a){
+  let d = 0;
+  let e = "";
+    while(d<a.length){
+     e=e+a[d]+"<br>";
+     d=d+1;
+    }
+  	return e;
+  };
+  const z = forEach([1,2,3]);
+  console.log(z);
+
+//ex5
+
+const sum = function (array){
+	let length = array.length;
+	let s = 0;
+	while (length > 0){
+		s = s + array[length-1];
+		length = length - 1;
+	}
+	return s;
+     };
+console.log (sum([1,2,3,4]))
+
+//ex6
+
+  const reverse = function (arr){
+  let ind = arr.length-1;
+    while(ind>=0){
+      console.log(arr[ind]);
+    ind = ind - 1;
+    }
+  };
+  reverse([1,2,3,4,5]);
